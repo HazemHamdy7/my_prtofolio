dbfd8aab0345dafda4109ea580f1e3c842d60bc3
Hazem Hamdy <hazemhamdy1215@gmail.com>
2025-04-15 22:54:29 +0200

    Add initial implementation of text styles and main application structure

---
+++ lib/my_app_bar.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:my_prtofolio/app_text_styles.dart';
+ 
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        AppLogo(),
+        Spacer(),
+        AppMenus(),
+        Spacer(),
+        LanguageToggle(),
+        ThemeToggle(),
+      ],
+    );
+  }
+}
+
+class AppLogo extends StatelessWidget {
+  const AppLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Portfolio', style: LargeTextStyles().titleLgBold);
+  }
+}
+
+class AppMenus extends StatelessWidget {
+  const AppMenus({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const [
+        Text('Home'),
+        Text('About'),
+        Text('Blog'),
+        Text('About Me'),
+      ],
+    );
+  }
+}
+
+class LanguageToggle extends StatelessWidget {
+  const LanguageToggle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton(
+      itemBuilder:
+          (context) => [
+            PopupMenuItem(child: Text('Arabic')),
+            PopupMenuItem(child: Text('English')),
+            PopupMenuItem(child: Text('French')),
+          ],
+    );
+  }
+}
+
+class ThemeToggle extends StatelessWidget {
+  const ThemeToggle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(value: false, onChanged: (value) {});
+  }
+}
