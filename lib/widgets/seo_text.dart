31748614ee01176984f28770822d81af373c9baf
Hazem Hamdy <hazemhamdy1215@gmail.com>
2025-04-20 21:13:10 +0200

    Add permissions for location, internet, and camera; implement HomeCoursesList and CoursesItem widgets; enhance SEOText with maxLines and overflow properties; update dependencies for permission handling

--- lib/widgets/seo_text.dart
+++ lib/widgets/seo_text.dart
@@ -8,15 +8,25 @@ class SEOText extends StatelessWidget {
     this.style,
     this.textAlign,
     this.textRenderStyle,
+    this.maxLines,
+    this.overflow,
   });
   final String text;
   final TextStyle? style;
   final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? overflow;
 
   final TextRendererStyle? textRenderStyle;
 
   @override
   Widget build(BuildContext context) {
-    return Text(text, textAlign: textAlign, style: style);
+    return Text(
+      text,
+      textAlign: textAlign,
+      style: style,
+      maxLines: maxLines,
+      overflow: overflow,
+    );
   }
 }
