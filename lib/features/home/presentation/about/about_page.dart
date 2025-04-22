637b456d2921f07d05c2cf5589c55766e5235256
Hazem Hamdy <hazemhamdy1215@gmail.com>
2025-04-22 23:58:25 +0200

    update AboutPage layout with new content and styling

--- lib/features/home/presentation/about/about_page.dart
+++ lib/features/home/presentation/about/about_page.dart
@@ -1,11 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:my_prtofolio/features/home/presentation/appbar/widget/app_scafflod.dart';
+import 'package:my_prtofolio/helper/extensions.dart';
+import 'package:my_prtofolio/shared/seo_text.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AppScafflod(slivers: []);
+    return AppScafflod(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: context.insets.padding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  radius: 100,
+                  backgroundImage: NetworkImage(
+                    'assets/images/me.jpg',
+                    scale: 2.5,
+                  ),
+                ),
+                Gap(16),
+                SEOText(
+                  context.texts.about_me,
+                  style: context.textStyle.titleSimeBold,
+                ),
+                Gap(16),
+                Text(
+                  context.texts.about_me_subtitle,
+                  style: context.textStyle.bodyLgMedium,
+                ),
+                Gap(35),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
